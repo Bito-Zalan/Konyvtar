20c25d039ba6a3278696f7dfc1b95f8d319f446b
Bito-Zalan <bitozalan23@gmail.com>
2024-11-25 10:39:36 +0100

    file-ok letrehozva

---
+++ src/main/java/hu/szamalk/Konyv.java
@@ -0,0 +1,19 @@
+package hu.szamalk;
+
+public class Konyv {
+
+    private String cim;
+    private String szerzo;
+    private int kiadaseve;
+    private int ar;
+
+    public Konyv(String cim, String szerzo, int kiadaseve, int ar) {
+        this.cim = cim;
+        this.szerzo = szerzo;
+        this.kiadaseve = kiadaseve;
+        this.ar = ar;
+    }
+
+
+}
+
